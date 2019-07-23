950f6cd51fe10da6044e44da597799cc6dd3d95e
feng <fl_zxf@sina.cn>
2019-07-23 16:12:30 +0800

    fasterxml test

---
+++ file-op-frame/faster-xml-test/readme.md
@@ -0,0 +1,2 @@
+# 用 faster-xml 映射 xml
+- 注解用法同 **jackson** 一样
---
+++ file-op-frame/faster-xml-test/src/main/java/cn/zxf/test/TestObject.java
@@ -0,0 +1,33 @@
+package cn.zxf.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class TestObject {
+
+    public static void main( String[] args ) throws IOException {
+        UserInfo user = UserInfo.builder()
+                .name( "zxf" )
+                .age( 22 )
+                .sex( "man" )
+                .build();
+
+        XmlMapper mapper = new XmlMapper();
+
+        String xml = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString( user );
+        System.out.println( xml );
+
+        File file = new File( "C:\\Users\\Administrator\\Desktop\\aa\\user.txt" );
+        mapper.writerWithDefaultPrettyPrinter()
+                .writeValue( file, user );
+
+        UserInfo user2 = mapper.readValue( xml, UserInfo.class );
+        System.out.println( user2 );
+        UserInfo user3 = mapper.readValue( file, UserInfo.class );
+        System.out.println( user3 );
+    }
+
+}
