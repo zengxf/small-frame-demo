e8957a9b5bbe28b3c2cc40bd137e44e9c704225d
feng <fl_zxf@sina.cn>
2019-10-28 17:39:22 +0800

    junit 5 测试

---
+++ junit-demo/junit5-test/src/test/java/ParameterizedUnitTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParameterizedUnitTest {
+
+    @ParameterizedTest
+    @ValueSource( ints = { 2, 4, 8 } )
+    void testNumberShouldBeEven( int num ) {
+        Assertions.assertEquals( 0, num % 2 );
+    }
+
+    @ParameterizedTest
+    @ValueSource( strings = { "Effective Java", "Code Complete", "Clean Code" } )
+    void testPrintTitle( String title ) {
+        System.out.println( title );
+    }
+
+    @ParameterizedTest
+    @CsvSource( { "1,One", "2, Two", "3 , Three" } )
+    void testDataFromCsv( long id, String name ) {
+        System.out.printf( "id: %d, name: %s", id, name );
+    }
+
+}
