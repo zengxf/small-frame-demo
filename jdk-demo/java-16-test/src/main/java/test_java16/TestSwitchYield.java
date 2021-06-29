0c032ca03074a5ce6a426001b912fbf29cb89351
ZXFeng <dev2068@sina.com>
2021-06-29 18:38:19 +0800

    Java 16 一些新特性测试

--- jdk-demo/java-16-test/src/main/java/test_java16/TestSwitchYield.java
+++ jdk-demo/java-16-test/src/main/java/test_java16/TestSwitchYield.java
@@ -3,7 +3,7 @@ package test_java16;
 /**
  * Java 13 引入 yield 关键字到 Switch 中。 <br/>
  * Switch 表达式中就多了一个关键字用于跳出 Switch 块的关键字 yield，主要用于返回一个值。 <br/>
- * yield和 return 的区别在于：return 会直接跳出当前循环或者方法，而 yield 只会跳出当前 Switch 块，
+ * yield 和 return 的区别在于：return 会直接跳出当前循环或者方法，而 yield 只会跳出当前 Switch 块，
  * 同时在使用 yield 时，需要有 default 条件。
  * <p>
  * 命令测试：
@@ -22,10 +22,11 @@ public class TestSwitchYield {
         String value = switch (name) {
             case "Java":
                 yield "object-oriented, platform independent and secured";
-            case "Ruby":
-                yield "a programmer's best friend";
-            default:
-                yield name + " is a good language";
+            case "Ruby", "C#": {
+                System.out.println("name: " + name);
+                yield "a programmer's best friend"; // 用于多行
+            }
+            default -> name + " is a good language";
         };
         return value;
     }
