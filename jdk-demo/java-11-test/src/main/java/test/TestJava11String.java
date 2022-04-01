29aeb6d2caed88818a9c886e3ad16166fe705723
ZXFeng <fa6688@sina.com>
2022-04-01 19:42:12 +0800

    UTF16 测试

--- jdk-demo/java-11-test/src/main/java/test/TestJava11String.java
+++ jdk-demo/java-11-test/src/main/java/test/TestJava11String.java
@@ -12,10 +12,27 @@ public class TestJava11String {
 
     public static void main(String[] args) throws InterruptedException {
         String en = "abc";
-        String zh = "中文";
+        String zh = "中文一";
+        String mix = "中abc";
         System.out.println(en);
         System.out.println(zh);
-        Thread.sleep(1800_000L);
+        System.out.println(mix);
+
+        String s1 = en.substring(0, 1);
+        System.out.println(s1);
+
+        s1 = zh.substring(0, 1);
+        System.out.println(s1);
+
+        s1 = mix.substring(0, 1);
+        System.out.println(s1);
+
+        // 相当于是 U+1D4D1
+        String u16 = "\uD835\uDCD1";    // https://www.compart.com/en/unicode/U+1D4D1
+        System.out.println(u16.length());
+        System.out.println(u16.substring(0, 1));
+
+        // Thread.sleep(1800_000L);
     }
 
 }
