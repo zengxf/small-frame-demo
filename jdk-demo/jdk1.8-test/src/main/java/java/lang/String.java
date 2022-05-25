476f6921a887a9ba4e83ec5a4b92a3ebbf5b382f
ZXFeng <fa6688@sina.com>
2022-05-25 09:59:18 +0800

    JDK 1.7 & 1.8 test

---
+++ jdk-demo/jdk1.8-test/src/main/java/java/lang/String.java
@@ -0,0 +1,6 @@
+package java.lang;
+
+// 不会被加载
+public class String {
+
+}
---
+++ jdk-demo/jdk1.8-test/src/main/java/test/base/TestString.java
@@ -0,0 +1,12 @@
+package test.base;
+
+public class TestString {
+
+    public static void main( String[] args ) {
+        String str = new String();
+        System.out.println( str );
+        java.lang.String str1 = new java.lang.String();
+        System.out.println( str1 );
+    }
+
+}
