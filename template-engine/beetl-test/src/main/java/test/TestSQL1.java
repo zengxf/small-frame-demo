ebcfbca8b8dc11008f1ab99846f76a659c14a48d
ZXFeng <fl_zxf@163.com>
2023-06-11 09:45:59 +0800

    Beetl 3 测试

--- template-engine/beetl-test/src/main/java/test/TestSQL1.java
+++ template-engine/beetl-test/src/main/java/test/TestSQL1.java
@@ -18,53 +18,50 @@ public class TestSQL1 {
         GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
 
         String fmt = """
-                -- 维修明细页
-                 SELECT
-                     md.asset_name AS assetName,            -- 资产名称
-                     md.basic_brand_name AS basicBrandName, -- 品牌
-                     md.basic_brand_model_name AS basicBrandModelName, -- 型号
-                     md.asset_number AS assetNumber,        -- 资产编码
-                     md.maintain_time AS maintainTime,      -- 维修时间
-                     md.maintain_number AS maintainNumber,  -- 维修单号
-                     md.maintain_amount AS maintainAmount,  -- 维修金额
-                     md.maintain_reason AS maintainReason,  -- 维修原因
-                     md.apply_person_name AS applyPersonName,   -- 维修申请人
-                     md.apply_person_duty AS applyPersonDuty,   -- 申请人岗位
-                     md.apply_department AS applyDepartment,    -- 申请人部门
-                     cfd.remark AS followupRemark,           -- 跟进内容
-                     cfd.followup_date AS followupDate,      -- 跟进时间
-                     cfd.followup_name AS followupName,      -- 跟进人名称
-                     cfs.num AS followupNum,                 -- 跟进记录数
-                     1 AS xxx
-                 FROM ads_ims_asset_maintain_detail AS md
-                     LEFT JOIN (
-                         SELECT\s
-                             business_type, business_str_id, MAX(id) newest_id, COUNT(*) num\s
-                         FROM bi_report_common_followup
-                         WHERE business_type = '10'
-                         GROUP BY business_type, business_str_id
-                     ) AS cfs    -- s 统计
-                         ON cfs.business_str_id = md.maintain_number
-                     LEFT JOIN bi_report_common_followup AS cfd  -- d 详情
-                         ON cfd.id = cfs.newest_id
-                 WHERE 1 = 1
-                     AND #'$genericQuery$'#      -- 通用查询以 md. 开头
-                     AND ${bdDataScope}
-                     <% if (!isEmpty(levelNum)) { %>  AND md.lv${levelNum}_id = ${departmentId} <% } %>
-                     <% if (isContain != 1) { %>  AND md.lv${levelNum + 1}_id != 0 <% } %>
-                 <% if (!isEmpty(orderByClauses)) { println(' ORDER BY ' + orderByClauses + ''); } %>
-                 <% if (isEmpty(orderByClauses)) { println(' ORDER BY departmentId DESC '); } %>
-                 <% if (isNotEmpty(export) && export == "1") {\s
-                     %>  LIMIT ${(_page - 1) * _pageSize}, ${_pageSize} <%\s
-                 } %>
+                -- 物料满意度明细页-查询 bigdata.ims.report.evaluationDetail.productSearch
+                SELECT
+                    md.query_month AS queryMonth,          -- 月份
+                    md.product_name AS productName,        -- 物料名称
+                    md.basic_brand_name AS basicBrandName, -- 品牌
+                    md.basic_brand_model_name AS basicBrandModelName, -- 型号
+                    md.product_number AS productNumber,    -- 料号
+                    md.user_satisfaction as userSatisfaction,           -- 使用满意度
+                    md.satisfaction_rating as satisfactionRating,       -- 满意度评级
+                    md.evaluation_description as evaluationDescription, -- 评价内容
+                    md.evaluation_person_name as evaluationPersonName,  -- 评价人名称
+                    md.evaluation_date as evaluationDate,               -- 评级时间
+                    1 AS xxx
+                FROM ads_ims.ads_ims_evaluation_detail AS md
+                WHERE 1 = 1
+                    AND #'$genericQuery$'#      -- 通用查询以 'md.' 开头
+                    AND ${bdDataScope}
+                    AND asset_number IS NULL
+                    <% if (!isEmpty(levelNum)) { %> \s
+                        AND md.lv${levelNum}_id = ${departmentId}\s
+                    <% } %>
+                    <% if (isContain != 1) { %>\s
+                        AND md.lv${levelNum + 1}_id != 0\s
+                    <% } %>
+                <% if (!isEmpty(orderByClauses)) { println(' ORDER BY ' + orderByClauses + ''); } %>
+                <% if (isEmpty(orderByClauses)) { println(' ORDER BY evaluationDate DESC'); } %>
+                <% if (isNotEmpty(export) && export == "1") { %> -- 相当于 MyBatis if 标签
+                LIMIT ${(_page - 1) * _pageSize}, ${_pageSize}\s
+                <% } %>
+                
+                <%
+                // 注释写在内部
+                /*
+                 * 块注释
+                 */
+                %>
                 """;
 
         // 获取模板
         Template t = gt.getTemplate(fmt);
         t.binding("bdDataScope", "1 = 1");
-        t.binding("levelNum", 1);
+        t.binding("levelNum", 4);
         t.binding("isContain", 2);
-        t.binding("departmentId", 60);
+        t.binding("departmentId", 73);
         t.binding("export", "1");
         t.binding("_page", 1);
         t.binding("_pageSize", 200);
