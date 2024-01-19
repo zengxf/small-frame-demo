489ea71f9868000e1265f8b8fa2a07abb415c241
ZXFeng <fl_zxf@163.com>
2024-01-19 15:06:30 +0800

    Liquibase 测试 2

---
+++ op-frame/java-op-db/liquibase-test/src/main/resources/v0.1/init-table.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO site_poster_apply(applicant_id, applicant_name, confirm_remark) VALUE(6688, 'Fa', 'test 1');
+INSERT INTO site_poster_apply(applicant_id, applicant_name, confirm_remark) VALUE(6688, 'Fa', 'test 2');
