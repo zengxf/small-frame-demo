72c0a50da1fc4a7c09814fe47f79a7ccbe663468
ZXFeng <fl_zxf@163.com>
2024-01-29 10:40:16 +0800

    Gradle 7 版本管理测试

---
+++ other-test/gradle-7-test/src/main/java/test/TestMain.java
@@ -0,0 +1,16 @@
+package test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p/>
+ * Created by ZXFeng on 2024/1/29
+ */
+@Slf4j
+public class TestMain {
+
+    public static void main(String[] args) {
+        log.info("Test OK");
+    }
+
+}
