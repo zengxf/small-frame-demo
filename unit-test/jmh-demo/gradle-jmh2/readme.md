3c56489d807ac78b3348954b3e97a9508fbb0c76
ZXFeng <fl_zxf@163.com>
2024-01-29 15:05:52 +0800

    Gradle JMH

--- unit-test/jmh-demo/gradle-jmh2/readme.md
+++ unit-test/jmh-demo/gradle-jmh2/readme.md
@@ -1,3 +1,8 @@
 ## 参考
-- IDEA-插件-详细介绍：https://github.com/artyushov/idea-jmh-plugin
+
 - IDEA-插件：https://plugins.jetbrains.com/plugin/7529-jmh-java-microbenchmark-harness
+    - 详细介绍：https://github.com/artyushov/idea-jmh-plugin
+- 可视化，可以试下这些网站：
+    - JMH Visual Chart：http://deepoove.com/jmh-visual-chart/
+    - JMH Visualizer：https://jmh.morethan.io/
+- JMH-介绍：https://hezhiqiang8909.gitbook.io/java/docs/javalib/jmh
--- unit-test/jmh-demo/gradle-jmh2/src/test/java/test/JmhTest.java
+++ unit-test/jmh-demo/gradle-jmh2/src/test/java/test/JmhTest.java
@@ -1,18 +1,31 @@
 package test;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p/>
  * Created by ZXFeng on 2024/1/29
  */
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(time = 1, iterations = 5)
+@Measurement(iterations = 5)
+@Threads(4)
+@Fork(1)
 public class JmhTest {
 
     // -Djava.io.tmpdir=D:\Data\Temp   // 可以不设置
     @Benchmark
     public void test1(Blackhole bh) {
         bh.consume(5);
+    }
+
+    @Benchmark
+    public void test2() {
+        NumUtils.sum(1, 2, 3, 4);
     }
 
 }
