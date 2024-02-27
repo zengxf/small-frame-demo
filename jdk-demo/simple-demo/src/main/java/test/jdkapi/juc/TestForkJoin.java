526de2b2017d0e2af0e280a881bd1c378f3fa925
ZXFeng <fl_zxf@163.com>
2024-02-27 10:56:50 +0800

    TestForkJoin

--- jdk-demo/simple-demo/src/main/java/test/jdkapi/juc/TestForkJoin.java
+++ jdk-demo/simple-demo/src/main/java/test/jdkapi/juc/TestForkJoin.java
@@ -7,26 +7,24 @@ import java.util.concurrent.RecursiveTask;
 
 /**
  * fork() 提交子任务，join() 合并子任务结果
- * 
- * <p>
+ * <p/>
  * Created by zengxf on 2018-03-02
  */
 public class TestForkJoin {
 
-    public static void main( String[] args ) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        CountTask task = new CountTask( 1, 4 );
-        Future<Integer> result = forkJoinPool.submit( task );
-        System.out.println( result.get() );
+        CountTask task = new CountTask(1, 4);
+        Future<Integer> result = forkJoinPool.submit(task);
+        System.out.println(result.get());
     }
 
     public static class CountTask extends RecursiveTask<Integer> {
-        private static final long serialVersionUID = 1L;
-        private static final int  THRESHOLD        = 2; // 阈值
-        private int               start;
-        private int               end;
+        private static final int THRESHOLD = 2; // 阈值
+        private int start;
+        private int end;
 
-        public CountTask( int start, int end ) {
+        public CountTask(int start, int end) {
             this.start = start;
             this.end = end;
         }
@@ -35,16 +33,16 @@ public class TestForkJoin {
         protected Integer compute() {
             int sum = 0;
             // 如果任务足够小就计算任务
-            boolean canCompute = ( end - start ) <= THRESHOLD;
-            if ( canCompute ) {
-                for ( int i = start; i <= end; i++ ) {
+            boolean canCompute = (end - start) <= THRESHOLD;
+            if (canCompute) {
+                for (int i = start; i <= end; i++) {
                     sum += i;
                 }
             } else {
                 // 如果任务大于阈值，就分裂成两个子任务计算
-                int middle = ( start + end ) / 2;
-                CountTask leftTask = new CountTask( start, middle );
-                CountTask rightTask = new CountTask( middle + 1, end );
+                int middle = (start + end) / 2;
+                CountTask leftTask = new CountTask(start, middle);
+                CountTask rightTask = new CountTask(middle + 1, end);
                 // 执行子任务
                 leftTask.fork();
                 rightTask.fork();
