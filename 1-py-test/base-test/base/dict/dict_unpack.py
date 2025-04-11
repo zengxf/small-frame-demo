74d88e37585b35aedae50ca7ce8356d050ebaeca
ZXFeng <fl_zxf@163.com>
2025-04-11 17:05:40 +0800

    Py 测试 kw 参数

--- 1-py-test/base-test/base/dict/dict_unpack.py
+++ 1-py-test/base-test/base/dict/dict_unpack.py
@@ -10,3 +10,6 @@ def example_function(a, b, c):
 print("\n-----------------------------------")
 kwargs = {'a': 1, 'b': 2, 'c': 3}
 example_function(**kwargs)
+
+print('kwargs:', kwargs)
+# print('kwargs:', **kwargs)  # 运行出错 >>> TypeError: print() got an unexpected keyword argument 'a'
--- 1-py-test/base-test/base/dict/fun_param_dict.py
+++ 1-py-test/base-test/base/dict/fun_param_dict.py
@@ -5,13 +5,21 @@
 """
 
 
-def dict_fun_test(a, b, **kw_args):
-    print('\n--------------------')
+def dict_fun_test1(a, b, **kw_args):
+    print('\n-------------------- 1')
     print(f"a: {a}")
     print(f"b: {b}")
     print(f"kw-args: {kw_args}")
 
 
-dict_fun_test(1, 2)
-dict_fun_test(2, 3, c=3, d=4, e=5)
-dict_fun_test(3, 4, str='abc', int=334, bol=True)
+# def dict_fun_test2(a, b, **kwargs, d):  # 编译出错
+def dict_fun_test2(a, b, **kwargs):
+    print('\n-------------------- 2')
+    print(f"a: {a}")
+    print(f"b: {b}")
+    print(f"kw-args: {kwargs}")
+
+
+dict_fun_test1(1, 2)
+dict_fun_test1(2, 3, c=3, d=4, e=5)
+dict_fun_test2(3, 4, str='abc', int=334, bol=True)
