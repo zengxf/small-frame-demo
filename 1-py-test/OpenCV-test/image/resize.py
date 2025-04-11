f081b1cb7b29e8465ebc5e900b1bc24e2807eeb2
ZXFeng <fl_zxf@163.com>
2025-04-11 11:54:49 +0800

    OpenCV test key

--- 1-py-test/OpenCV-test/image/resize.py
+++ 1-py-test/OpenCV-test/image/resize.py
@@ -9,12 +9,18 @@ import read_img
 image = read_img.test_img()
 cv2.imshow('img-src', image)  # 显示原图
 
-resized = cv2.resize(image, (300, 300))  # 缩放
+# 缩放
+resized = cv2.resize(image, (300, 300))
 cv2.imshow('resized', resized)
 
+# 读取长宽
 (h, w) = image.shape[:2]
-matrix = cv2.getRotationMatrix2D((w // 2, h // 2), 45, 1.0)  # 旋转45度
+print('h', h, 'w', w)
+
+# 旋转45度
+matrix = cv2.getRotationMatrix2D((w // 2, h // 2), 45, 1.0)
 rotated = cv2.warpAffine(image, matrix, (w, h))
 cv2.imshow('Rotated', rotated)
 
-cv2.waitKey(0)
+key = cv2.waitKey(0)
+print('key:', key)
