fd42bdf2e7c6e0a3454473ab146ccd51c6824c20
ZXFeng <fl_zxf@163.com>
2025-04-11 17:40:51 +0800

    Py 测试 dict 合并

--- 1-py-test/base-test/base/dict/dict_unpack.py
+++ 1-py-test/base-test/base/dict/dict_unpack.py
@@ -13,3 +13,11 @@ example_function(**kwargs)
 
 print('kwargs:', kwargs)
 # print('kwargs:', **kwargs)  # 运行出错 >>> TypeError: print() got an unexpected keyword argument 'a'
+
+
+# 合并测试
+defaults = {"a": 0.25, "b": 0.45, "save": False}
+final_args1 = {**defaults, **kwargs}  # 后面会覆盖前面的
+final_args2 = {**kwargs, **defaults}  # 后面会覆盖前面的
+print('final_args1:', final_args1)
+print('final_args2:', final_args2)
