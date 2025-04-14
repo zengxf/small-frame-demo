cecd2affaca1569b0433cec8e34907c9aa17043c
ZXFeng <fl_zxf@163.com>
2025-04-14 15:37:56 +0800

    Py 测试 继承

--- 1-py-test/base-test/class_obj/class_extends/child.py
+++ 1-py-test/base-test/class_obj/class_extends/child.py
@@ -2,13 +2,18 @@
 子类
 """
 
-from parent import Parent
+from parent import Parent, Parent2
 
 
-class Child(Parent):
+# 表示 Child 继承 Parent, Parent2
+class Child(Parent, Parent2):
     def test2(self):
         print("ok2", self.__class__)
-        self.test()
+        self.test_p1()
+        self.test_p2()
+        super().test_p1()   # 显示调用父类方法
+        super().test_p2()   # 显示调用父类方法
+        print('super', super().__class__)
 
 
 c = Child()
--- 1-py-test/base-test/class_obj/class_extends/parent.py
+++ 1-py-test/base-test/class_obj/class_extends/parent.py
@@ -4,8 +4,13 @@
 
 
 class Parent(object):
-    def test(self):
-        print("ok1", self.__class__)
+    def test_p1(self):
+        print("p ok1", self.__class__)
+
+
+class Parent2(object):
+    def test_p2(self):
+        print("p ok2", self.__class__)
 
 # p = Parent()
 # p.test()
