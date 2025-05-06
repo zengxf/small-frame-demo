cc13ba52c8774d8b71548495ff3e5d31547fd2fd
ZXFeng <fl_zxf@163.com>
2025-05-06 18:08:28 +0800

    C++ 空指针访问成员函数

---
+++ 1-c-test/base-test/b_core/null_vis_member_fun.cpp
@@ -0,0 +1,44 @@
+//
+// Created by ZXF on 2025/5/6.
+//
+/*
+  空指针访问成员函数
+  C++中空指针也是可以调用成员函数的，但是也要注意有没有用到this指针；
+  如果用到this指针，需要加以判断保证代码的健壮性。
+ */
+
+#include <iostream>
+#include <string>
+using namespace std; // Added using directive
+
+class person {
+public:
+    void showClassName() {
+        std::cout << "我是person类" << std::endl;
+        cout << "<UNK>person<UNK>" << std::endl;
+    }
+
+    void showPerson() {
+        std::cout << "show --> 1" << std::endl;
+        // std::cout << "show --> " << mAge << std::endl; // err
+        if (this == NULL) {
+            return;
+        }
+        std::cout << "show --> " << mAge << std::endl;
+    }
+
+public:
+    int mAge;
+};
+
+void test01() {
+    person *p = NULL;
+    p->showClassName(); // 空指针，可以调用成员函数
+    p->showPerson(); // 如果成员函数中用到了this指针，就不可以了
+}
+
+int main() {
+    test01();
+    // system("pause");
+    return 0;
+}
