8b9d9d4f71581c164aec7820a7728f3ab5c86aaf
ZXFeng <fl_zxf@163.com>
2025-07-02 09:54:20 +0800

    AI study - open cv

--- 1-py-test/ai_study/img/opencv/temp_img.py
+++ 1-py-test/ai_study/img/opencv/temp_img.py
@@ -1,77 +1,73 @@
 import cv2
 import numpy as np
-
+import random
 
 def show(img, title=''):
     cv2.imshow(title, img)
 
+# 随机模糊
+def rnd_blur(image):
+    k_size = random.choice([25, 35])
+    ratio = random.uniform(0, 1)
+    print(f'rnd_blur, k_size: {k_size}, ratio: {ratio}')
 
-path = r"./imgs/lenaNoise.png"
-path = r"D:/Data/Test/img/qianming.png"
-img = cv2.imread(path, 0)
+    blur_img = None
+    if ratio < 0.3:
+        blur_img = cv2.medianBlur(image, k_size)
+    if ratio >= 0.3 and ratio < 0.6:
+        blur_img = cv2.blur(image, (k_size, k_size))
+    if ratio >= 0.6:
+        blur_img = cv2.GaussianBlur(image, (k_size, k_size), 0)
+    return blur_img
+
+# 将 2 张图片进行 混合
+def mix(img1, img2):
+    h, w = img1.shape[:2]  # 读 img 1 的 h w
+    img2 = cv2.resize(img2, [w, h])  # 对 img 2 进行大小调整
+    # last_img = (img2 * 0.5 + img1 * 0.5).astype(np.uint8)
+    last_img = cv2.addWeighted(img2, 0.5, img1, 0.5, 0)  # 相当于上面的封装
+    return last_img
+
+# 先模糊，再将模糊的图与原图进行 混合
+def blur_mix(img1):
+    img2 = rnd_blur(img1)
+    ratio = random.uniform(0.3, 0.7)
+    print(f'blur_mix, ratio: {ratio}')
+
+    last_img = cv2.addWeighted(img2, ratio, img1, 1 - ratio, 0)
+    return last_img
+
+# path1 = r"./imgs/lenaNoise.png"
+path1 = r"D:/Data/Test/img/lenaNoise.png"
+# path2 = r"./imgs/ye2.jpg"
+path2 = r"D:/Data/Test/img/ye2.jpg"
+img = cv2.imread(path1, 1)
+img2 = cv2.imread(path2, 1)
 # show(img, 'src_img')
 
 # - -
 
-# 高斯
-gas_blur = cv2.GaussianBlur(img, (5, 5), 0)
-concat_gas_blur = np.hstack([img, gas_blur])
-show(concat_gas_blur, 'concat_gas_blur')
+# 随机
+rnd_blur_img = rnd_blur(img)
+concat_rnd_blur = np.hstack([img, rnd_blur_img])
+show(concat_rnd_blur, 'concat_rnd_blur')
 
 # - -
 
-# 黑白 (转黑白)
-# ret, ths1_img = cv2.threshold(gas_blur, 30, 255, cv2.THRESH_BINARY)
-ret, ths1_img = cv2.threshold(gas_blur, 127, 255, cv2.THRESH_BINARY)
-# show(ths1_img, 'ths1_img')
-concat_ths1_img = np.hstack([img, ths1_img])
-show(concat_ths1_img, 'concat_ths1_img')
+# 两图混合 1
+mix_img1 = mix(img, img2)
+concat_mix_img = np.hstack([img, mix_img1])
+show(concat_mix_img, 'concat_mix_img 1')
+
+mix_img2 = mix(img2, img)
+concat_mix_img = np.hstack([img2, mix_img2])
+show(concat_mix_img, 'concat_mix_img 2')
 
 # - -
 
-# 腐蚀
-yuan = np.array([
-    [0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-], dtype=np.uint8)
-erosion = cv2.erode(ths1_img, yuan, iterations=1)  # 腐蚀
-concat_erosion = np.hstack([img, erosion])
-show(concat_erosion, 'concat_erosion 5')
-
-kernel = np.ones((3, 3), np.uint8)
-erosion = cv2.erode(ths1_img, kernel, iterations=2)  # 腐蚀
-concat_erosion = np.hstack([img, erosion])
-show(concat_erosion, 'concat_erosion 3')
-
-# - -
-
-# 膨胀
-dilate_img = cv2.dilate(ths1_img, kernel, iterations=1)  # 膨胀
-concat_dilate_img = np.hstack([img, dilate_img])
-show(concat_dilate_img, 'concat_dilate_img')
-
-# - -
-
-# 黑白 - 腐蚀  = 得到边缘，或者空心效果
-sub_erosion_img = np.abs(ths1_img - erosion)
-concat_sub_erosion_img = np.hstack([img, sub_erosion_img])
-show(concat_sub_erosion_img, 'ths1 sub erosion')
-
-# - -
-
-# 黑白 - 膨胀 = 有可能是空心效果
-sub_dilate_img = np.abs(ths1_img - dilate_img)
-concat_sub_dilate_img = np.hstack([img, sub_dilate_img])
-show(concat_sub_dilate_img, '(concat 1) ths1 sub dilate')
-concat_sub_dilate_img = np.hstack([ths1_img, dilate_img, sub_dilate_img])
-show(concat_sub_dilate_img, '(concat 2) ths1 sub dilate')
-
-# - -
-
-# 膨胀 - 腐蚀 = 或者空心效果。边缘
-show(np.hstack([img, np.abs(dilate_img - erosion)]), 'dilate sub erosion')
+# 模糊混合
+blur_mix_img = blur_mix(img)
+concat_blur_mix = np.hstack([img, blur_mix_img])
+show(concat_blur_mix, 'concat_blur_mix')
 
 cv2.waitKey(0)
