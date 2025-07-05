0abadfea08c6076b68b8bc35a4d3e6963cc0a966
ZXFeng <fl_zxf@163.com>
2025-07-06 05:41:21 +0800

    AI study - knn

---
+++ 1-py-test/ai_study/arithmetic/knn算法的实现.py
@@ -0,0 +1,116 @@
+# 第2章/machineLearn/knn算法的实现.py
+import math
+import matplotlib.pyplot as plt
+
+amount_ratio = 1000
+# 历史相亲数据的描述
+def historical_data():
+    # 所有金额缩小到K为单位，方便计算
+    data = {
+        # 姓名：[年龄，收入，是否成功(0为失败，1为成功)]
+        '文*': [25, 5000 / amount_ratio, 0],
+        '祁*': [30, 20000 / amount_ratio, 1],
+        '杨*1': [28, 11000 / amount_ratio, 0],
+        '万*': [30, 40000 / amount_ratio, 1],
+        '余*': [32, 6000 / amount_ratio, 0],
+        '张*': [21, 7000 / amount_ratio, 0],
+        '陈*': [27, 4000 / amount_ratio, 0],
+        '利*': [26, 14000 / amount_ratio, 1],
+        '杨*2': [34, 24000 / amount_ratio, 1],
+        '钱*': [36, 7000 / amount_ratio, 0],
+    }
+    return data
+
+
+def calculate_european_distance(input_x: list, data: dict):
+    """
+    返回欧式距离后的结果，并以[[人名,距离]]的格式返回
+    :param input_x:
+    :param data:
+    :return:
+    """
+    distance_list = []
+    for key, v in data.items():
+        # 计算欧式距离
+        d = math.sqrt((input_x[0] - v[0]) ** 2 + (input_x[1] - v[1]) ** 2)
+        distance_list.append([key, round(d, 2)])
+    # 对计算后的距离结果进行升序排列
+    # 假设有10条数据，那么计算后的结果就是10条。即每一个数据都有一个距离
+    return sorted(distance_list, key=lambda l: l[1])
+
+
+def predict_ByK(distance_list, raw_data, k=5):
+    # 确定前k个样本所在类别出现的频率，并输出出现频率最高的类别
+    labels = {"相亲失败": 0, "相亲成功": 0}
+    for s in distance_list[:k]:
+        # 根据上一步距离计算出来的人名，从原始数据中取出样本信息
+        # 即：[25, 5000 / amount_ratio, 0]
+        sample_information = raw_data[s[0]]
+        if sample_information[-1] == 0:
+            labels['相亲失败'] += 1
+        elif sample_information[-1] == 1:
+            labels['相亲成功'] += 1
+    # 根据相亲结果出现的次数进行升序排列。
+    return sorted(labels.items(), key=lambda l: l[1], reverse=True)
+
+
+def guess_k(lenged=0.4):
+    """利用已有数据来对K值进行验证。"""
+    raw_data = historical_data()
+    data_keys = raw_data.keys()
+    data_values = [[k] + v for k, v in raw_data.items()]
+    # 通常说来取源数据的40%。
+    data_values = data_values[0:int(len(data_keys) * lenged)]
+    max_k = len(data_keys)
+    verfiy_k = []
+    verfiy_error = []
+    verfiy_wrong = []
+    for k in range(1, max_k, 2):
+        error_count = 0
+        guss_count = 0
+        for d in data_values:
+            label = '相亲失败' if d[-1] == 0 else '相亲成功'
+            distance_list = calculate_european_distance(d[1:3], raw_data)
+            result = predict_ByK(distance_list, raw_data, k=k)
+            if label != result[0][0]:
+                error_count += 1
+            guss_count += 1
+        verfiy_k.append(k)
+        verfiy_error.append(error_count)
+        verfiy_wrong.append(round(error_count / guss_count, 2))
+    return verfiy_k, verfiy_error, verfiy_wrong
+
+
+def plot_k_error(error, start=1, end=10, step=2):
+    # 用来正常显示中文标签
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    # 将x周的刻度线方向设置向内
+    plt.rcParams['xtick.direction'] = 'in'
+    # 将y轴的刻度方向设置向内
+    plt.rcParams['ytick.direction'] = 'in'
+    fig = plt.figure(num=10)
+    ax1 = fig.add_subplot(111)
+    ax1.plot(range(start, end, step), error[-1])
+    ax1.set_xlabel(r'$K$')
+    ax1.set_ylabel('错误率')
+    for a, b in zip(range(start, end, step), error[-1]):
+        plt.text(a, b, b, ha='center', va='bottom', fontsize=8)
+    plt.show()
+
+
+def test():
+    raw_data = historical_data()
+    input_x = ['小文', 26, 13000 / amount_ratio]
+    distance_list = calculate_european_distance(input_x[1:], raw_data)
+    result = predict_ByK(distance_list, raw_data, k=5)
+    print(f'预测{input_x[0]}的相亲结果为：{result[0][0]}，\n '
+          f'概率为：{round(result[0][1] / sum([y for x, y in result]), 2)}')
+
+
+if __name__ == "__main__":
+    # knn的算法过程
+    test()
+    # 测试，K取那一个值能够在验证集中效果最佳。数据较少，所以这里取所有
+    # error = guess_k(lenged=1)
+    # plot_k_error(error)
---
+++ 1-py-test/ai_study/arithmetic/一元线性回归梯度下降求解.py
@@ -0,0 +1,89 @@
+# 第2章/machineLearn/一元线性回归梯度下降求解.py
+import matplotlib.pyplot as plt
+
+
+# 预测函数
+def prediction(w, x, b):
+    return w * x + b
+
+
+# 损失函数
+def sum_loss(X, Y, w, b):
+    total_loss = 0
+    for x, y in zip(X, Y):
+        total_loss += (y - prediction(w, x, b)) ** 2
+    return 1 / (2 * len(X)) * total_loss
+
+
+# w的梯度，使用了全量梯度
+def sum_gradient_w(X, Y, w, b):
+    total_w = 0
+    for x, y in zip(X, Y):
+        total_w += (prediction(w, x, b) - y) * x
+    return 1 / len(X) * total_w
+
+
+# b的梯度，使用了全量梯度
+def sum_gradient_b(X, Y, w, b):
+    total_b = 0
+    for x, y in zip(X, Y):
+        total_b += (prediction(w, x, b) - y) * 1
+    return 1 / len(X) * total_b
+def plt_gradient_linear_process(X, Y, result_wb):
+    # 用来正常显示中文标签
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    # 将x周的刻度线方向设置向内
+    plt.rcParams['xtick.direction'] = 'in'
+    # 将y轴的刻度方向设置向内
+    plt.rcParams['ytick.direction'] = 'in'
+    plt.scatter(X, Y)
+    length = len(result_wb)
+    num = int(length * 0.2)
+    mid_wb = result_wb[num]
+    last_wb = result_wb[-1]
+    plt.plot(X, [mid_wb[0] * x + mid_wb[1] for x in X], label='学习第{}次拟合直线'.format(num),linestyle='--')
+    plt.plot(X, [0.4645 * x + 35.97 for x in X], label='最佳拟合直线')
+    plt.plot(X, [last_wb[0] * x + last_wb[1] for x in X], label='学习第{}次拟合直线'.format(length),linestyle=':')
+    # plt.title("父亲身高x（时）与儿子身高y（时）的关系\n梯度下降线性拟合过程")
+    plt.xlabel('父亲身高x（时）')
+    plt.ylabel('儿子身高y（时）')
+    plt.legend(loc='upper left')
+    plt.text(x=69,
+             y=70,
+             s=f'temp_w={init_wb[0]}\n'
+               f'temp_b={init_wb[1]}\n'
+               f'learning={learning}\n'
+               f'epoch={epoch}\n'
+               f'loss={round(loss, 4)}',
+             fontdict=dict(fontsize=8, family='monospace', ),  # 字体属性字典
+             # 添加文字背景色
+             bbox={'facecolor': '#74C476',  # 填充色
+                   'edgecolor': 'b',  # 外框色
+                   'alpha': 0.8,  # 框透明度
+                   'pad': 0.9,  # 本文与框周围距离
+                   }
+             )
+    plt.savefig('loss_lr.png')
+    plt.show()
+
+if __name__ == "__main__":
+    epoch = 21  # 迭代次数
+    init_wb = 1, 40  # 初始参数值 迁移学习
+    temp_w, temp_b = init_wb[0], init_wb[1]
+    learning = 0.0001  # 学习率
+    X = [60, 62, 64, 65, 66, 67, 68, 70, 72, 74]
+    Y = [63.6, 65.2, 66, 65.5, 66.9, 67.1, 67.4, 68.3, 70.1, 70]
+    result_wb = []  # 存储过程中的梯度
+    for item in range(epoch):
+        # 梯度下降
+        w = temp_w - learning * sum_gradient_w(X, Y, temp_w, temp_b)
+        b = temp_b - learning * sum_gradient_b(X, Y, temp_w, temp_b)
+        loss = sum_loss(X, Y, w, b)
+        if item % 5 == 0:
+            print('第{}次梯度,(w,b)参数为({:6f},{:6f}),总损失{:6f}'.format(item + 1, w, b, loss))
+        # 更新后再赋值给temp，这样能保证w、b同时更新
+        temp_w = w
+        temp_b = b
+        result_wb.append([temp_w, temp_b])
+    plt_gradient_linear_process(X, Y, result_wb)
